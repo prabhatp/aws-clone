a468293321f0aae0ad213ec08e59f6dc0a60123a
= <=>
2020-08-23 16:26:31 +0530

    Updates

---
+++ precache-manifest.033a1089980dc7751ffa22b0cbed4a9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "780d696906ce65d56d16cef652a26293",
+    "url": "/aws-clone/index.html"
+  },
+  {
+    "revision": "a37821adb83df0ae73df",
+    "url": "/aws-clone/static/css/main.dd6aa329.chunk.css"
+  },
+  {
+    "revision": "ff9339a90eb4326de00a",
+    "url": "/aws-clone/static/js/2.b6df1096.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/aws-clone/static/js/2.b6df1096.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a37821adb83df0ae73df",
+    "url": "/aws-clone/static/js/main.304a96de.chunk.js"
+  },
+  {
+    "revision": "f49ceb74312a2670b06f",
+    "url": "/aws-clone/static/js/runtime-main.7bb84d49.js"
+  }
+]);
